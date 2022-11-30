07b64f777f049dda12085ffa9339b8e607e5c9e0
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-30 17:11:44 +0100

    fix server.php

--- server.php
+++ server.php
@@ -9,5 +9,10 @@
 //     'Laravel'
 // ];
 
-$to_do = file_get_contents('package.json');
-$to_do_array = json_decode($to_do);
+$getTask = file_get_contents('package.json');
+$taskList = json_decode($getTask);
+var_dump($taskList);
+var_dump($getTask);
+// header('Content')
+// $to_do = file_get_contents('package.json');
+// $to_do_array = json_decode($to_do);s
